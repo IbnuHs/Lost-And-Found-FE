49ad680f4981a05c020d7558d1786470b38c3961
IbnuHs <ibnuhs30@gmail.com>
2024-07-22 14:32:22 +0800

    .

--- src/lib/API.js
+++ src/lib/API.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
-  // baseURL: " https://ade1-103-85-117-17.ngrok-free.app",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://2dc5-103-85-117-17.ngrok-free.app",
   headers: {
     "Content-type": "application/json",
     Accept: "application/json",
